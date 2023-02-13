8d69a3b8433cf2966306bd8aa98ba2d093cb3cab
Kosma Kurlowicz <kosma.kurlowicz@gmail.com>
2023-02-13 21:55:54 +0100

    Rewrites router ti import views as components.

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AddProductView from '../views/AddProductView.vue'
+import Error404 from '../views/404.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,15 +14,12 @@ const router = createRouter({
     {
       path: '/add-product',
       name: 'add-product',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AddProductView.vue')
+      component: AddProductView
     },
     {
       path: '/:pathMatch(.*)',
       name: 'not-found',
-      component: () => import('../views/404.vue')
+      component: Error404
     }
   ]
 })
